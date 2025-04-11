5c67e4346a3bbc4171844134943c3c426a0a54df
MiguelMascarini <m.valhiente@aluno.ifsp.edu.br>
2025-04-11 10:51:29 -0300

    Update codaulaarrays.js

--- codaulaarrays.js
+++ codaulaarrays.js
@@ -68,3 +68,31 @@ console.log(`A aluna no índice 1 se chama ${lista[0][1]}
 
 //percorrer listas
 console.log(lista)
+
+
+
+//exercicio
+function concatArrays(array1, array2) {
+    return array1.concat(array2);
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const parteNumeros = numeros.slice(3, 8);
+
+
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi'];
+
+frutas.splice(2, 2, 'Kiwi', 'Pêssego');
+
+
+const menuPrincipal = ['Hambúrguer', 'Pizza', 'Lasanha'];
+const menuDeSobremesas = ['Sorvete', 'Pudim', 'Bolo'];
+
+const menuCompleto = menuPrincipal.concat(menuDeSobremesas);
+
+
+console.log("Array concatenado:", concatArrays([1, 2, 3], [4, 5, 6]));
+console.log("Parte dos números:", parteNumeros);
+console.log("Frutas atualizadas:", frutas);
+console.log("Menu completo:", menuCompleto);
